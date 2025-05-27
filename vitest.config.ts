62c48bef5ee636daae3aac307f50f4cccc91faac
Ogura Daiki <8hachibee125@gmail.com>
2025-05-27 10:51:12 +0900

    copy path alias from tsconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,17 @@
+import path from "path";
+
 import {defineConfig} from 'vitest/config';
+
+import baseConf from './tsconfig.base.json';
+
+const alias = Object.entries(baseConf.compilerOptions.paths).reduce((buf, [k, v]) => {
+  buf[k] = path.resolve(__dirname, v[0]);
+  return buf;
+} ,{})
 
 export default defineConfig({
   test: {
+    alias,
     globals: true,
     environment: 'node',
     include: ['src/tests/**/*.test.{ts,js}', 'packages/*/src/**/*.test.{ts,js}'],
